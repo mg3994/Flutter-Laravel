ce96f2d9d1d125648361318ad85ee0dab4194220
mg3994 <126542773+mg3994@users.noreply.github.com>
2024-06-17 13:16:55 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -22,7 +22,7 @@ in {
       cp -rf ${flutter} flutter
       chmod -R u+w flutter
       PUB_CACHE=/tmp/pub-cache ./flutter/bin/flutter create --org com.antinna --project-name "$WS_NAME" "$out"/"$WS_NAME"_flutter
-      export COMPOSER_HOME= ${composerPath}
+      # COMPOSER_HOME=${composerPath
       ${composerPath} create-project --prefer-dist laravel/laravel "$out"/"$WS_NAME"_laravel
       # mkdir -p "$out"/.{flutter-sdk,idx}
       # mv flutter "$out/.flutter-sdk/flutter"
